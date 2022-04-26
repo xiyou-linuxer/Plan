26e20f2d54a278e1efbf825831ee0e869a3f63b0
Y7n05h <Y7n05h@protonmail.com>
2022-04-26 21:55:12 +0800

    Update

--- old-Plan/TaskQueue.c
+++ old-Plan/TaskQueue.c
@@ -1,13 +1,15 @@
+// Single-producer , single-consumer Queue
 struct SPSCQueue{
     /* Define Your Data Here */
 
 }typedef SPSCQueue;;
-SPSCQueue *SPSCQueueInit(int threadNumber);
+SPSCQueue *SPSCQueueInit();
 void SPSCQueuePush(SPSCQueue *pool, void *s);
 void *SPSCQueuePop(SPSCQueue *pool);
 void SPSCQueueDestory(SPSCQueue *);
 
 
+//Multi-producer , Multi-consumer Queue
 struct MPMCQueue {
     /* Define Your Data Here */
 } typedef MPMCQueue;
