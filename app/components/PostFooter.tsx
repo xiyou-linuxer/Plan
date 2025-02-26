d7b33b8f528b882afd08d8912cb66c23a5d4e509
L33Z22L11 <L33Z22L11@gmail.com>
2025-02-26 18:27:47 +0800

    fix(website): metadata

--- app/components/PostFooter.tsx
+++ app/components/PostFooter.tsx
@@ -1,7 +1,8 @@
 export default function PostFooter() {
   return (
-    <footer className="post-footer p-6 my-6 bg-gray-300/20 text-opacity-50 rounded-lg">
-      文章使用 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans" rel="noopener noreferrer" target="_blank">CC BY-NC-SA 4.0</a> 协议。
+    <footer className="post-footer relative p-6 my-6 bg-gray-400/10 border border-gray-400/10 rounded-lg">
+      <span className="type-song absolute top-0 right-0 text-4xl text-gray-400/20 pointer-events-none">培养计划</span>
+      文章使用 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans" rel="noopener noreferrer nofollow" target="_blank">CC BY-NC-SA 4.0</a> 协议。
     </footer>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,29 @@ import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 import './global.css'
 
+const title = '西邮 Linux 兴趣小组培养计划'
+
 export const metadata = {
   // https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+  // https://github.com/shuding/nextra/blob/main/docs/app/layout.tsx
+  metadataBase: new URL('https://plan.xiyoulinux.com/'),
+  generator: 'Next.js',
+  applicationName: title,
+  appleWebApp: { title },
+  title: {
+    default: title,
+    template: `%s | ${title}`,
+  },
+  openGraph: {
+    url: './',
+    siteName: title,
+    locale: 'zh',
+    type: 'website',
+  },
+  alternates: {
+    // https://github.com/vercel/next.js/discussions/50189#discussioncomment-10826632
+    canonical: './',
+  },
 }
 
 const head = <></>
@@ -15,7 +36,7 @@ const projectLink = 'https://github.com/xiyou-linuxer/Plan'
 const navbar = (
   <Navbar
     align="left"
-    logo={<strong className="type-song">培养计划</strong>}
+    logo={<span className="type-song">培养计划</span>}
     projectLink={projectLink}
     chatLink="https://www.xiyoulinux.com/"
     chatIcon={<span>主站</span>}
@@ -25,7 +46,7 @@ const navbar = (
 const footer = (
   <Footer className="gap-4">
     <span>© {new Date().getFullYear()} 西邮 Linux 兴趣小组</span>
-    <a href="https://beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">陕ICP备2023007680号-1</a>
+    <a href="https://beian.miit.gov.cn" target="_blank" rel="noopener noreferrer nofollow">陕ICP备2023007680号-1</a>
   </Footer>
 )
 
@@ -48,7 +69,9 @@ export default async function RootLayout({ children }) {
             title: '目录',
             backToTop: '回到顶部',
           }}
-          feedback={{ content: '向本页提出Issue' }}
+          feedback={{
+            content: '提出Issue',
+          }}
           editLink="编辑本页"
           footer={footer}
         >
